de75a50bac9c18e917cfef21226ba4bb907f9f1a
Kalyb Prince <kalybprince91@gmail.com>
2025-05-30 15:40:11 -0500

    Intitialized errorsInChains.js

---
+++ javascriptSyntax/promises/errorsInChains.js
@@ -0,0 +1,6 @@
+/* Handling Errors in Chains (`.catch()`)
+
+`catch()` is used to handle error that occur anywhere in the
+Promise chain.  When a Promise in the chain is rejected,
+constrol immediately jumps to the nearest `catch` handler. */
+
